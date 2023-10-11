699e729c7ee6895cd6ec6a9c3c142620edefe3ee
Stannt <97431057+Stannt@users.noreply.github.com>
2023-10-11 18:07:24 +0200

    route (#2)
    * route
    
    * settingss
    
    ---------
    
    Co-authored-by: jb <jb.aubareau@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import Pong from "./pages/Pong";
+import Settings from "./pages/Settings";
+
 export function App() {
     return (
-        <div>
-            testdsadsasdsadsa
-        </div>
+       <BrowserRouter>
+			<Routes>
+				<Route path='*' element={<Home />} />
+				<Route path="home" element={<Home />} />
+				<Route path="user" element={<User />} />
+				<Route path="pong" element={<Pong />} />
+				<Route path="settings" element={<Settings />} />
+			</Routes>
+       </BrowserRouter>
     )
 }
