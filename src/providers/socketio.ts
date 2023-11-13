1f357a213ca26e8c479fe182cb4278908cbd34a8
Kevin Mendes <kmendes@c3r2s5.42lausanne.ch>
2023-11-13 12:10:14 +0100

    svaepoint

--- src/providers/socketio.ts
+++ src/providers/socketio.ts
@@ -1,3 +1,9 @@
+import { getAccessToken } from "@/technical/AccessTokenManager";
+import { isDef } from "@/technical/isDef";
 import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io("localhost:3000");
+export const socket: Socket = io(import.meta.env.VITE_API_URL, {
+    query: {
+        token: isDef(getAccessToken()) ? `Bearer ${getAccessToken()}` : undefined
+    }
+});
