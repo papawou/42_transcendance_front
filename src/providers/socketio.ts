694e80593d85c889a1781ed0721a836b066b2643
Kevin Mendes <kmendes@c3r2s5.42lausanne.ch>
2023-11-14 08:19:53 +0100

    socketio

--- src/providers/socketio.ts
+++ src/providers/socketio.ts
@@ -1,9 +1,6 @@
 import { getAccessToken } from "@/technical/AccessTokenManager";
-import { isDef } from "@/technical/isDef";
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(import.meta.env.VITE_API_URL, {
-    query: {
-        token: isDef(getAccessToken()) ? `Bearer ${getAccessToken()}` : undefined
-    }
+    auth: (cb) => cb({ token: getAccessToken() })
 });
