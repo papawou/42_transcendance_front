3af565bec5346d7e3ac872fdbde136dffa6e87e0
Kevin Mendes <kmendes@c3r2s5.42lausanne.ch>
2023-11-20 23:37:18 +0100

    boilerplate

--- src/pages/Boilerplate.tsx
+++ src/pages/Boilerplate.tsx
@@ -1,17 +1,30 @@
 import Navigation from "@/components/Navigation/Navigation";
-import { ProtectedChildren } from "@/components/ProtectedChildren";
+import { useIsLogged } from "@/components/ProtectedChildren";
 import { SocialPanel } from "@/components/SocialPanel";
-
+import { Chat } from "@/components/chat/Chat";
 import { Outlet } from "react-router-dom";
 
+
 export const Boilerplate = () => {
+    const isLogged = useIsLogged()
+
+    if (!isLogged) {
+        return <Outlet />
+    }
     return (
-        <>
-            <ProtectedChildren>
-                <Navigation />
-                <SocialPanel />
-            </ProtectedChildren>
-            <Outlet />
-        </>
+        <div>
+            <Navigation />
+            <div style={{ display: "flex" }}>
+                <div style={{ flex: "0 1 auto", border: "1px solid blue" }}>
+                    <SocialPanel />
+                </div>
+                <div style={{ flex: "1 1 auto", border: "1px solid green" }}>
+                    <Outlet />
+                </div>
+                <div style={{ flex: "0 1 auto", border: "1px solid red" }}>
+                    <Chat />
+                </div>
+            </div>
+        </div>
     )
 }
