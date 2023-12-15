3d9f6138138fa58aa314fade5e426f89cd79e71a
MateyN <mateynnikolov@gmail.com>
2023-12-15 20:13:41 +0100

    fixed bad request but cant get qrcode / secret staticly typed

--- src/components/TwoFactorAuth/Enable2fa.tsx
+++ src/components/TwoFactorAuth/Enable2fa.tsx
@@ -1,40 +1,48 @@
-import { useState, ChangeEvent, useEffect } from "react";
+import { useState, ChangeEvent } from "react";
 import axiosInstance from "@/services/AxiosInstance";
+import { useAuth } from "@/components/providers/AuthProvider";
 
 export function EnableTwoFactor() {
-  const [code, setCode] = useState("");
+  const { user } = useAuth();
   const [qrCodeImage, setQrCodeImage] = useState("");
   const [secretKey, setSecretKey] = useState("");
 
-  const handleCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCode(e.target.value);
+  const handleSecretKeyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSecretKey(e.target.value);
   };
 
   const handleSubmit = () => {
-    axiosInstance
-      .post("auth/2fa/enable", { code, secretKey })
-      .then((res) => {
-        console.log("2FA enabled:", res.data.message);
-        if (res.data && res.data.qrCodeImage && res.data.secretKey) {
-          setQrCodeImage(res.data.qrCodeImage);
-          setSecretKey(res.data.secretKey);
-        }
-      })
-      .catch((err) => {
-        console.error("Failed to enable 2FA:", err);
-      });
+    if (user && user.id) {
+      axiosInstance
+        .post("auth/2fa/enable", { userId: user.id, secretKey: secretKey, name: user.name, qrCodeImage: qrCodeImage })
+        .then((res) => {
+          console.log("2FA enabled:", res.data.message);
+          if (res.data && res.data.qrCodeImage && res.data.secretKey) {
+            setQrCodeImage(res.data.qrCodeImage);
+            console.log("QR Code Image:", qrCodeImage);
+            setSecretKey(res.data.secretKey);
+          }
+        })
+        .catch((err) => {
+          console.error("Failed to enable 2FA:", err);
+        });
+    }
   };
 
   return (
     <div>
       <h2>Enable Two-Factor Authentication</h2>
-      <input type="text" value={code} onChange={handleCodeChange} />
+      <input
+        type="text"
+        value={secretKey}
+        onChange={handleSecretKeyChange}
+        placeholder="Enter secret key"
+      />
       <button onClick={handleSubmit}>Enable 2FA</button>
       {qrCodeImage && (
         <div>
           <h3>Scan this QR Code</h3>
           <img src={qrCodeImage} alt="QR Code" />
-          <p>Secret Key: {secretKey}</p>
         </div>
       )}
     </div>
