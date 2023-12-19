7a3427433642536388ba5346a9031e00704d7a5f
MateyN <mateynnikolov@gmail.com>
2023-12-19 02:55:26 +0100

    css for loginpage test

--- src/components/Unlogged/LoginPage.tsx
+++ src/components/Unlogged/LoginPage.tsx
@@ -1,18 +1,21 @@
 import { useCallback } from "react";
+import "./LoginPage.css";
+import Logo42 from "../../../public/42_logo.png";
 
 const API_FT_OAUTH2_URL = `${import.meta.env.VITE_API_FT_URL}/oauth/authorize?client_id=${import.meta.env.VITE_API_FT_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_API_FT_CALLBACK_URL)}&response_type=code`;
-
 
 export const LoginPage = () => {
     const handleClick = useCallback(() => {
         window.location.href = API_FT_OAUTH2_URL;
-    }, [])
+    }, []);
 
     return (
-        <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "50px" }}>
-            <button onClick={() => handleClick()}>
-                LOGIN API 42
+        <div className="container">
+            <div className="ball"></div>
+            <h1 className="title">Couz Transcendence</h1>
+            <button className="login-button" onClick={handleClick}>
+            LOGIN API <img src={Logo42} alt="Logo" className="logo-in-button" />
             </button>
         </div>
-    )
-}   
+    );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+
+const fullReloadAlways: PluginOption = {
+	name: 'full-reload-always',
+	handleHotUpdate({ server }) {
+	  server.ws.send({ type: "full-reload" })
+	  return []
+	},
+  } as PluginOption
 
 export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 5173,
-		strictPort: true
+		strictPort: true,
+		watch: {
+			usePolling: true
+		  }
 	},
-	plugins: [react()],
+	plugins: [react(), fullReloadAlways],
 	resolve: {
-		alias: {
+		alias: { 
 			'@': path.resolve(__dirname, './src'),
 		}
 	}
