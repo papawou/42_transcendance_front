4fb032b1e512a8112babddea96fc10a5225768db
MateyN <mateynnikolov@gmail.com>
2023-12-20 14:38:28 +0100

    2fa snackbar loginpage

--- src/components/Unlogged/TfaAccess.tsx
+++ src/components/Unlogged/TfaAccess.tsx
@@ -2,12 +2,14 @@ import axiosInstance from "@/services/AxiosInstance"
 import { useCallback, useState } from "react"
 import { useAuth } from "../providers/AuthProvider"
 import { useSearchParams } from "react-router-dom"
+import { useSnackbar } from 'notistack';
 
 export function TfaAccess() {
     const { login } = useAuth()
     const [searchParams,] = useSearchParams()
     const [tfaCodeActivate, setTfaCodeActivate] = useState<string | undefined>("")
     const [error, setError] = useState(false)
+    const { enqueueSnackbar } = useSnackbar();
 
     const handleCodeActivateChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
@@ -18,13 +20,15 @@ export function TfaAccess() {
         setError(false);
         axiosInstance.post("auth/tfa/verify", { userId: Number(searchParams.get("userId")), otp: tfaCodeActivate })
             .then(res => login(res.data.access_token))
-            .catch(() => setError(true))
-    }, [login, searchParams, tfaCodeActivate])
+            .catch(() => {
+                setError(true);
+                enqueueSnackbar('Wrong OTP! Try again!', { variant: 'error' });
+            });
+    }, [login, searchParams, tfaCodeActivate, enqueueSnackbar])
 
     return (
         <div>
-            {error && <p>Error OTP</p>}
-            <input placeholder='code ici' onChange={handleCodeActivateChange} value={tfaCodeActivate} />
+            <input placeholder='Enter OTP here' onChange={handleCodeActivateChange} value={tfaCodeActivate} />
             <button onClick={handleCodeActivate}>ENVOYER</button>
         </div>
     )
